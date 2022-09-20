9105afee968c96c9bc5c1bdd3d5904f39ff0f510
EktaMahor <mahorekta@gmail.com>
2022-09-20 10:41:08 +0530

    Initial commit

---
+++ portfolio/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=250)
+    image = models.ImageField(upload_to='portfolio/images/')
+    url = models.URLField(blank=True, max_length=200)
+
+    from django.db import models
+
+    def __str__(self):
+        return self.title
